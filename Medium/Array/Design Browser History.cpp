3e756a708927cc4ffe509b9c4e51b05f4ea887d9
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2023-03-18 12:16:19 +0530

    Create Design Browser History.cpp

---
+++ Medium/Array/Design Browser History.cpp
@@ -0,0 +1,25 @@
+class BrowserHistory {
+public:
+ vector<string>history;
+    int curr; 
+    BrowserHistory(string homepage) {
+        history.push_back(homepage);
+        curr = 0;
+    }
+    
+    void visit(string url) {
+        history.erase(history.begin()+curr+1,history.end());
+        history.push_back(url);
+        curr++;
+    }
+    
+    string back(int steps) {
+        curr = max(0,curr-steps);
+        return history[curr];
+    }
+    
+    string forward(int steps) {
+        curr = min((int)history.size()-1,curr+steps);
+        return history[curr];
+    }
+};
