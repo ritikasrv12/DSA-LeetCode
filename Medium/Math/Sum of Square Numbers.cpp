5e74033e08ae2d2bff55f7862386e4f72d546278
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2023-03-21 20:06:06 +0530

    Create  Sum of Square Numbers.cpp

---
+++ Medium/Math/Sum of Square Numbers.cpp
@@ -0,0 +1,12 @@
+long long left = 0, right = sqrt(c);
+        while (left <= right) {
+            long long sum = left * left + right * right;
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return false;
