1db47878154dd17832d08cbe2d230435095512c7
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2023-05-30 17:19:26 +0530

    Create Design HashSet.cpp

---
+++ Easy/Array/Design HashSet.cpp
@@ -0,0 +1,19 @@
+class MyHashSet {
+public:
+int arr[10000001];
+    MyHashSet() {
+        memset(arr,-1,sizeof(arr));
+    }
+    
+    void add(int key) {
+        arr[key]=1;
+    }
+    
+    void remove(int key) {
+        arr[key]=-1;
+    }
+    
+    bool contains(int key) {
+        return arr[key]==1;
+    }
+};
