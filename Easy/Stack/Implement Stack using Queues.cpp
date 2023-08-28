444d90c1018554bd36a321b444be12febf7b90af
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2023-08-28 17:38:10 +0530

    Create  Implement Stack using Queues.cpp

---
+++ Easy/Stack/Implement Stack using Queues.cpp
@@ -0,0 +1,36 @@
+class MyStack {
+public:
+   queue<int>q1,q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        //q1->q2
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        //q1 push x
+        q1.push(x);
+
+        //q2->q1
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+    }
+    
+    int pop() {
+        int val=q1.front();
+        q1.pop();
+        return val;
+    }
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
