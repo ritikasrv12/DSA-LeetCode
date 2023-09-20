9ba9675bb8f88c5418fa77630b8d1f7628a31a3e
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2023-09-20 22:25:29 +0530

    Create Minimum Operations to Reduce X to Zero.cpp

---
+++ Medium/Array/Minimum Operations to Reduce X to Zero.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        int target = totalSum - x;
+        if (target < 0)
+            return -1;
+        if (target == 0)
+            return nums.size();
+        int n = nums.size();
+        int minOperations = INT_MAX;
+        int currentSum = 0;
+        int leftIndex = 0, rightIndex = 0;
+        while (rightIndex < n) {
+            currentSum += nums[rightIndex];
+            rightIndex++;
+            while (currentSum > target && leftIndex < n) {
+                currentSum -= nums[leftIndex];
+                leftIndex++;
+            }
+            if (currentSum == target)
+                minOperations = min(minOperations, n - (rightIndex - leftIndex));
+        }
+        return (minOperations == INT_MAX) ? -1 : minOperations;
+        
+    }
+};
