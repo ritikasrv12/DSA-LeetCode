ed4b8e731533951fb9034ba15895e31dc473d81f
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2023-11-15 23:11:20 +0530

    Maximum Element After Decreasing and Rearranging.cpp

---
+++ Medium/Array/Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int prev=0, n=arr.size();
+        for(auto& el : arr){
+            el = min(el, prev+1);
+            prev = el;
+        }
+        return arr[n-1];
+        
+    }
+};
