22454b8c2e74bba71dc94d4679a6b80537d536b7
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2024-01-21 23:43:43 +0530

    House Robber.cpp

---
+++ Medium/Array/House Robber.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[0];
+        } else if (n == 2) {
+            return max(nums[0], nums[1]);
+        }
+
+        // dp[i] represents the maximum amount of money that can be robbed up to the i-th house
+        vector<int> dp(n, 0);
+        
+        // Base cases
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        // Dynamic programming
+        for (int i = 2; i < n; ++i) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+        
+    }
+};
