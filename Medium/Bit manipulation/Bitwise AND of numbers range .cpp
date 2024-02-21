4054e4cff93c2ebdc7b1c4079e55d541dc090b13
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2024-02-21 18:36:18 +0530

    Bitwise AND of numbers range .cpp

---
+++ Medium/Bit manipulation/Bitwise AND of numbers range .cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        // Find the common prefix
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        // Shift back to get the result
+        return left << shift;
+        
+    }
+};
