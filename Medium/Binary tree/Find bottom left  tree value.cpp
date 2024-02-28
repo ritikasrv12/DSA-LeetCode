1143b7bc02e5a8904256d8d0280021fb84458ca2
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2024-02-28 18:37:48 +0530

    Find bottom left  tree value.cpp

---
+++ Medium/Binary tree/Find bottom left  tree value.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        if (!root) return 0; // Handle empty tree
+        
+        std::queue<TreeNode*> q;
+        q.push(root);
+        int leftmostValue = 0; // Initialize with default value
+        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            leftmostValue = q.front()->val; // Update leftmost value for each level
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        
+        return leftmostValue;
+        
+    }
+};
