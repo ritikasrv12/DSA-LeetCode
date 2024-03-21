d677bc8ed6b5c41295ed33996f19853d767f1976
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2024-03-21 19:37:52 +0530

    Reverse linked list.cpp

---
+++ Easy/Linked List/Reverse linked list.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* current = head;
+        ListNode* nextNode = nullptr;
+        
+        while (current != nullptr) {
+            nextNode = current->next; // Store the next node
+            current->next = prev; // Reverse the pointer
+            prev = current; // Move pointers one position ahead
+            current = nextNode;
+        }
+        
+        // At the end of the loop, 'prev' will be the new head
+        return prev;
+        
+    }
+};
